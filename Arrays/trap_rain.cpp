f78f09dcbd895ab08dd6556ef7efaeca55cbf85b
Samuthrika Shree S <samuthrikashrees@gmail.com>
2026-02-09 21:44:53 +0530

    Trap rain water

---
+++ Arrays/trap_rain.cpp
@@ -0,0 +1,32 @@
+// LeetCode 42 - Trapping Rain Water
+// https://leetcode.com/problems/trapping-rain-water/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int leftmax = 0, rightmax = 0, water = 0;
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftmax) {
+                    leftmax = height[left];
+                } else {
+                    water += (leftmax - height[left]);
+                }
+                left++;
+            } 
+            else {
+                if (height[right] >= rightmax) {
+                    rightmax = height[right];
+                } else {
+                    water += (rightmax - height[right]);
+                }
+                right--;
+            }
+        }
+
+        return water;
+    }
+};
