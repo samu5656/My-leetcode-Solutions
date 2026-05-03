e070c998958d0ee71202613ef94925827e54aa32
Samuthrika Shree S <samuthrikashrees@gmail.com>
2026-05-03 19:38:05 +0530

    close string

---
+++ string/close_strings.cpp
@@ -0,0 +1,33 @@
+// LeetCode: Determine if Two Strings Are Close
+// Approach: Check same character set and compare sorted frequency arrays.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if (word1.length() != word2.length())
+            return false;
+
+        vector<int> freq1(26, 0), freq2(26, 0);
+
+        for (char c : word1)
+            freq1[c - 'a']++;
+
+        for (char c : word2)
+            freq2[c - 'a']++;
+
+        for (int i = 0; i < 26; i++) {
+            if ((freq1[i] == 0 && freq2[i] != 0) ||
+                (freq1[i] != 0 && freq2[i] == 0)) {
+                return false;
+            }
+        }
+
+        sort(freq1.begin(), freq1.end());
+        sort(freq2.begin(), freq2.end());
+
+        return freq1 == freq2;
+    }
+};
