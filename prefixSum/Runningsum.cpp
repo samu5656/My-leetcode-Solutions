9d36bf1ac0e1f8086d8f16dfb1af7040f01d50ab
Samuthrika Shree S <samuthrikashrees@gmail.com>
2026-06-10 21:36:58 +0530

    Running sum of 1d array

---
+++ prefixSum/Runningsum.cpp
@@ -0,0 +1,15 @@
+// LeetCode 1480: Running Sum of 1d Array
+// https://leetcode.com/problems/running-sum-of-1d-array/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            nums[i] = nums[i - 1] + nums[i];
+        }
+        return nums;
+    }
+};
